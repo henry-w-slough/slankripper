0ad0bc1a3059304580a3aad4715722684a7d6b96
henry-w-slough <henry.w.slough@gmail.com>
2026-04-07 09:55:13 -0400

    Changed some comments

--- Client/Client.py
+++ Client/Client.py
@@ -7,15 +7,9 @@ class Client():
 
 
     def __init__(self) -> None:
-        
-        #the size of which chunks are created (default 32MB)
-        self.read_size = (1024*1024*32)
-
-
-    def set_read_size(self, size:int) -> None:
-        """Sets the size in megabytes at which file chunks will be created."""
-        #NOTE: May want to change this for more versatility of chunk data size
-        self.read_size = (1024*1024*size)
+        """Manager of finding, copying, and handling Repository data."""
+        #there will be other functionality relating to clients needing class vars
+        pass
 
 
     def copy_repository_data(self, repository_dir:str, new_dir:str) -> None:
@@ -25,4 +19,4 @@ class Client():
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
         
-        FileReader.copy_repository_data(repository_dir, new_dir, self.read_size)
+        FileReader.copy_repository_data(repository_dir, new_dir)
--- Client/FileReader.py
+++ Client/FileReader.py
@@ -4,7 +4,7 @@ import json
 from .. import config
 
 
-def copy_repository_data(repository_dir:str, new_dir:str, read_size:int) -> None:
+def copy_repository_data(repository_dir:str, new_dir:str) -> None:
     """Takes a repository directory and attaches all it's data chunks back together, puts the result in the new directory. Also migrates the manifest.json for metadata."""
 
     #getting chunk order
@@ -22,7 +22,7 @@ def copy_repository_data(repository_dir:str, new_dir:str, read_size:int) -> None
         #writing each chunk in order
         for chunk in chunk_order:
             with open(os.path.join(repository_dir, config.DATA_DIR, chunk), "rb") as chunk_file:
-                new_file.write(chunk_file.read(read_size))
+                new_file.write(chunk_file.read())
 
     
 
